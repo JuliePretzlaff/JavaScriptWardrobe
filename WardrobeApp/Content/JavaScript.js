4884a96ad1d89bfcee130a89a6b1f6207a79fb1d
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-23 14:35:22 -0400

    JS search function working

--- WardrobeApp/Content/JavaScript.js
+++ WardrobeApp/Content/JavaScript.js
@@ -2,28 +2,20 @@
 //function to open search option
 function searchItem() {
     var itemSeason = prompt("What season would you like to view?");
-    //below won't work by class name, only ID
-    //if change below to class name, use no inner text, and add seasonClass[i]innertext to for loop
     var seasonClass = document.getElementsByClassName("season");
     var nameClass = document.getElementsByClassName("itemName");
     //alert(seasonClass);
     
-
-    //var itemSeason = prompt("What season would you like to view?");
-    //var seasonClass = document.getElementsByInnerText(itemSeason);
-    //alert('hello');
-
-
     var match = "Your results: ";
 
     for (var i = 0; i < seasonClass.length; i++) {
         if (seasonClass[i].innerText == itemSeason) {
-            alert(i)
-            //match = itemName[i].innertext;
+           // alert(i)
+            alert(nameClass[i].innerText);
             
         }
     }
-    alert(match);
+    //alert(match);
 }
 
 
