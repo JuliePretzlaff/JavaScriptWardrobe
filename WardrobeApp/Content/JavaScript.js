294dc68f6ccb20dbaa522297f2177d4f0bb02611
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-23 10:56:43 -0400

    initial commit of wardrobe project to new github repo

---
+++ WardrobeApp/Content/JavaScript.js
@@ -0,0 +1,7 @@
+﻿
+//function to open search option
+function SearchItem(){
+    var itemType = prompt('What type of item would you like to search for? (Please select from: tops, bottoms, shoes, accessories)');
+    var itemSeason = prompt('What season would you like to view ' + itemType + ' for?');
+
+}
